befa1740cfa5939527ca0cf323070329e28ac4a5
野村 修 <nomura@tricogimmick.com>
2018-04-10 10:34:36 +0900

    平均、ヒストグラム、相対度数の追加

---
+++ statistics/statistics/Statistics/Statistics.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hsb.Statistics
+{
+    #region 【Statistics】
+    /// <summary>
+    /// 統計ライブラリ
+    /// </summary>
+    public static class Statistics
+    {
+        #region - Average : 平均を求める
+        /// <summary>
+        /// 平均を求める
+        /// </summary>
+        /// <typeparam name="T">型パラメータ</typeparam>
+        /// <param name="source">投入データ</param>
+        /// <param name="convert">投入データから平均値を求める値を返す関数</param>
+        /// <returns>平均値</returns>
+        public static double Average<T>(this IEnumerable<T> source, Func<T,double> convert)
+        {
+            var count = 0;
+            var sum = 0d;
+            foreach (var data in source)
+            {
+                count++;
+                sum += convert(data);
+            }
+            return sum / count;
+        }
+        #endregion
+
+        #region - Histogram : ヒストグラムを取得する
+        /// <summary>
+        /// ヒストグラムを取得する
+        /// </summary>
+        /// <typeparam name="T">型パラメータ</typeparam>
+        /// <param name="source">投入データ</param>
+        /// <param name="convert">投入データからヒストグラム求める値を返す関数</param>
+        /// <param name="start">開始値</param>
+        /// <param name="step">度数</param>
+        /// <param name="size">個数</param>
+        /// <returns>ヒストグラム</returns>
+        public static int[] Histogram<T>(this IEnumerable<T> source, Func<T, double> convert,
+                                                 double start, double step, int size)
+        {
+            var histogram = new int[size];
+            foreach (var data in source)
+            {
+                var i = (int)Math.Floor((convert(data) - start) / step);
+                if (i >= 0 && i < size)
+                    histogram[i]++;
+            }
+            return histogram;
+        }
+        #endregion
+
+        #region - RelativeFrequency : 相対度数を取得する　
+        /// <summary>
+        /// 相対度数を取得する
+        /// </summary>
+        /// <typeparam name="T">型パラメータ</typeparam>
+        /// <param name="source">投入データ</param>
+        /// <param name="convert">投入データから相対度数を求める値を返す関数</param>
+        /// <returns>相対度数</returns>
+        public static double[] RelativeFrequency<T>(this IEnumerable<T> source, Func<T, double> convert, int digits = 2)
+        {
+            var sum = source.Sum(convert);
+            return source.Select(data => Math.Round(convert(data) / sum, digits)).ToArray();
+        }
+        #endregion
+    }
+    #endregion
+}
---
+++ statistics/statisticsTests/Statistics/StatisticsTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hsb.Statistics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hsb.Statistics.Tests
+{
+    #region 【Test Class】
+    [TestClass()]
+    public class StatisticsTests
+    {
+        #region - AverageTest
+        /// <summary>
+        /// Test of Average
+        /// </summary>
+        [TestMethod()]
+        public void AverageTest()
+        {
+            var access = new int[]
+            {
+                354, 351, 344, 362, 327, 349, 361, 360, 333, 366, 343, 349, 358, 373, 334,
+                338, 355, 329, 370, 324, 387, 370, 357, 342, 338, 320, 359, 308, 323, 338
+            };
+            Func<int, double> cv = (n) => (double)n;
+            Assert.AreEqual(347.4d, Statistics.Average(access, cv));
+            Assert.AreEqual(347.6d, Statistics.Average(access.Take(5), cv));
+        }
+        #endregion
+
+        #region - HistogramTest
+        /// <summary>
+        /// Test of Histogram
+        /// </summary>
+        [TestMethod()]
+        public void HistogramTest()
+        {
+            var access = new int[]
+            {
+                354, 351, 344, 362, 327, 349, 361, 360, 333, 366, 343, 349, 358, 373, 334,
+                338, 355, 329, 370, 324, 387, 370, 357, 342, 338, 319, 359, 308, 323, 338
+            };
+            var expected = new int[]
+            {
+                0, 1, 1, 4, 5, 5, 6, 4, 3, 1, 0
+            };
+            Func<int, double> cv = (n) => (double)n;
+            Assert.IsTrue(expected.SequenceEqual(
+                Statistics.Histogram(access, cv, 290.0d, 10.0d, 11)));
+        }
+        #endregion
+
+        #region - RelativeFrequencyTest
+        /// <summary>
+        /// Test of RelativeFrequency
+        /// </summary>
+        [TestMethod()]
+        public void RelativeFrequencyTest()
+        {
+            var histogram = new int[]
+            {
+                0, 1, 1, 4, 5, 5, 6, 4, 3, 1, 0
+            };
+            var expected = new double[]
+            {
+                0d, 0.033d, 0.033d, 0.133d, 0.167d, 0.167d, 0.2d, 0.133d, 0.1d, 0.033, 0d
+            };
+            Func<int, double> cv = (n) => (double)n;
+            Assert.IsTrue(expected.SequenceEqual(
+                Statistics.RelativeFrequency(histogram, cv, 3)));
+        }
+        #endregion
+    }
+    #endregion
+}
